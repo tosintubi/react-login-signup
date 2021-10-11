af6ce59443d2c8ef7d74262b0f8f1e27cc8e9014
Tosin <tosintubi@users.noreply.github.com>
2021-10-11 21:42:01 +0200

    changed from Class component to functional components

--- src/components/login.compnent.js
+++ src/components/login.compnent.js
@@ -6,22 +6,15 @@ const Login =() => {
             <form>
                 <h3>Sign In</h3>
 
-                <div className="form-group">
-                    <label>Email address</label>
                     <input type="email" className="form-control" placeholder="Enter email" />
-                </div>
+                
 
-                <div className="form-group">
-                    <label>Password</label>
+            
                     <input type="password" className="form-control" placeholder="Enter password" />
-                </div>
 
-                <div className="form-group">
-                    <div className="custom-control custom-checkbox">
-                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                    </div>
-                </div>
+                    <input type="checkbox" className="custom-control-input" id="customCheck" />
+                    <label className="custom-control-label" htmlFor="customCheck">Remember me</label>
+                    
 
                 <button type="submit" className="btn btn-primary btn-block">Submit</button>
                 <p className="forgot-password text-right">
