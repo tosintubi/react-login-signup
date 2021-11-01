1c6fdc9552ac5e1653687725cf826d47fb9f7cc9
Tosin <tosintubi@users.noreply.github.com>
2021-11-01 23:57:47 +0100

    Google Login

---
+++ src/components/google.component.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import { GoogleLogin } from 'react-google-login';
+// refresh token
+import { refreshTokenSetup } from '../utils/refreshToken';
+
+const clientId =
+  '170044535313-f02c8hd020ptha7n0t4fraouo2ap9bqq.apps.googleusercontent.com';
+
+function LoginGoogle() {
+  const onSuccess = (res) => {
+    console.log('Login Success: currentUser:', res.profileObj);
+    console.log('Login Success: Token', res.tokenObj);
+    alert(
+      `Logged in successfully welcome ${res.profileObj.name} 😍. \n See console for full profile object.`
+    );
+    refreshTokenSetup(res);
+  };
+
+  const onFailure = (res) => {
+    console.log('Login failed: res:', res);
+    alert(
+      `Failed to login. 😢 Please ping this to repo owner twitter.com/sivanesh_fiz`
+    );
+  };
+
+  return (
+    <div>
+      <GoogleLogin
+        clientId={clientId}
+        buttonText="Login with Google"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        cookiePolicy={'single_host_origin'}
+        style={{ marginTop: '100px' }}
+        isSignedIn={true}
+      />
+    </div>
+  );
+}
+
+export default LoginGoogle;
--- src/components/login.compnent.js
+++ src/components/login.compnent.js
@@ -1,26 +1,33 @@
 import React from "react";
 import imgPlaceHolder from '../images/img-placeholder.jpg';
+import LoginGoogle from "./google.component";
+import LogoutGoogle from "./google.logout.component";
+
 
 const Login =() => {
         return (
-            <form>
-                <h3>Sign In</h3>
+            <div>
+                <LoginGoogle/>
 
-                    <input type="email" className="form-control" placeholder="Enter email" />
-                
+                <LogoutGoogle/>
 
+            </div>
             
-                    <input type="password" className="form-control" placeholder="Enter password" />
+            // <form>
+            //     <h3>Sign In</h3>
 
-                    <input type="checkbox" className="custom-control-input" id="customCheck" />
-                    <label className="custom-control-label" htmlFor="customCheck">Remember me</label>
-                    
+            //         <input type="email" className="form-control" placeholder="Enter email" />
+                
+            //         <input type="password" className="form-control" placeholder="Enter password" />
 
-                <button type="submit" className="btn btn-primary btn-block">Submit</button>
-                <p className="forgot-password text-right">
-                    Forgot <a href="#">password?</a>
-                </p>
-            </form>
+            //         <input type="checkbox" className="custom-control-input" id="customCheck" />
+            //         <label className="custom-control-label" htmlFor="customCheck">Remember me</label>
+
+            //     <button type="submit" className="btn btn-primary btn-block">Submit</button>
+            //     <p className="forgot-password text-right">
+            //         Forgot <a href="#">password?</a>
+            //     </p>
+            // </form>
         );
 }
 
